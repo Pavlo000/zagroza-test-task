d2bf82dac6b26f0c6cbb5357b2cd9e8988f8f164
Pasha Moroz <pmoroz2004@gmail.com>
2023-05-28 18:18:34 +0300

    create base version

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { About } from "./components/About";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Testimonials } from "./components/Testimonials";
+
+import './App.scss';
+import { Contacts } from "./components/Contacts";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      App
+      <Header />
+      <main>
+        <div className="App__section container">
+          <Testimonials />
+        </div>
+        <div className="App__section container">
+          <About />
+        </div>
+        <div className="App__section App__section--background">
+          <div className="container">
+            <Contacts />
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
